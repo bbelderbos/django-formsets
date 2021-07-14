d37cc7f3b91b07944274c468e88d6d676ed40f66
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-14 16:35:25 +0200

    add to admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'authors', 'description',
+                    'image_link', 'inserted')
+    search_fields = ('title', 'authors', 'description')
+
+
+admin.site.register(Book, BookAdmin)
